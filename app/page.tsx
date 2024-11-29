53e80a3fb17260f6e1d95dec8f6a6ff9c2c6f822
paulelite <paulelitehonour@gmail.com>
2024-11-29 06:36:10 -0800

    multi-step form

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import ProgressBar from "./svgComponents/components/ProgressBar";
 
 export default function Home() {
-  const [cartTitle, setCartTitle] = useState("add to cart");
 
   return (
     <div className="flex flex-col gap-10 justify-center items-center h-screen p-10">
