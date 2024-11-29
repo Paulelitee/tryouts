d2d2585d8afe6d3112b941b7ad9d09da8871f8cb
paulelite <paulelitehonour@gmail.com>
2024-11-29 06:34:56 -0800

    multi-step form

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,6 @@ import ProgressBar from "./svgComponents/components/ProgressBar";
 export default function Home() {
   const [cartTitle, setCartTitle] = useState("add to cart");
 
- 
-    function handleClick() {
-      if (cartTitle === "add to cart") {
-        setCartTitle("checkout");
-      } else {
-        setCartTitle("add to cart");
-      }
-    }
-    
-
   return (
     <div className="flex flex-col gap-10 justify-center items-center h-screen p-10">
       <ProgressBar />
